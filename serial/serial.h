94909d339d352c37f547eb490d209c0b31333a88
LitQ <fang770489306@gmail.com>
2017-07-10 16:28:16 +0800

    增加新的文件

---
+++ serial/serial.h
@@ -0,0 +1,9 @@
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+extern int serial_open(const char* serialName,int nSpeed);
+extern int serial_close(int fd);
+extern long serial_read(int fd, unsigned char *buf, int len);
+extern int serial_write(int fd, unsigned char *buf, int len);
+
+#endif
